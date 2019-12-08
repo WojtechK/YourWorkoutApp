d6d44b5fefe1e9d540ea83f24708986527e81f08
wojciech.kaczor <wojtek7297@gmail.com>
2019-12-08 18:37:59 +0100

    intro view layout finished (mobile and desktop)

---
+++ src/app/intro/intro.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.scss']
+})
+export class IntroComponent implements OnInit {
+  readonly title: string = 'your workout';
+  readonly description: string = 'personal training web application';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
